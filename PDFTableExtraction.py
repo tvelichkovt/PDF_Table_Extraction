1c880c6848000fef709bd7be6277298f7eae657d
Todor <tvelichkovt@gmail.com>
2020-03-06 10:47:59 +0200

    Update PDFTableExtraction.py

--- PDFTableExtraction.py
+++ PDFTableExtraction.py
@@ -1,2 +1,20 @@
-#pip install camelot,click;matplotlib;numpy;opencv-python;openpyxl;pandas;pdfminer.six;PyPDF2
+#pip install camelot-py[all],click;matplotlib;numpy;opencv-python;openpyxl;pandas;pdfminer.six;PyPDF2
 
+
+
+import camelot
+tables = camelot.read_pdf('C:\@\T\git\PDF_Table_Extraction\foo.pdf')
+tables
+
+tables.export('foo.csv', f='csv', compress=True) # json, excel, html
+
+
+tables[0].parsing_report
+{
+    'accuracy': 99.02,
+    'whitespace': 12.24,
+    'order': 1,
+    'page': 1
+}
+tables[0].to_csv('foo.csv') # to_json, to_excel, to_html
+tables[0].df # get a pandas DataFrame!

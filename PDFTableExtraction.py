9d066a049900aec22e85ab983628d19123b6b1b5
Todor <tvelichkovt@gmail.com>
2020-03-12 10:14:43 +0200

    Update PDFTableExtraction.py

--- PDFTableExtraction.py
+++ PDFTableExtraction.py
@@ -11,17 +11,22 @@ from tabula import read_pdf
 pdf_file1 = "https://github.com/tvelichkovt/PDF_Table_Extraction/raw/master/Sample_Files/PDFTableExtraction_Small_Table.pdf"
 pdf_file2 = "https://github.com/tvelichkovt/PDF_Table_Extraction/raw/master/Sample_Files/PDFTableExtraction_Tesla_Bonds.pdf"
 pdf_file3 = "https://github.com/tvelichkovt/PDF_Table_Extraction/raw/master/Sample_Files/PDFTableExtraction_World_GDP.pdf"
-pdf_file4 = "https://databank.worldbank.org/data/download/GDP.pdf"
+pdf_file4 = "https://github.com/tvelichkovt/PDF_Table_Extraction/raw/master/Sample_Files/PDFTableExtraction_Cars.pdf"
+pdf_file5 = "https://databank.worldbank.org/data/download/GDP.pdf"
 
 # 2. Read PDFs
 
 tabula.read_pdf(pdf_file1, pages="all", stream=True)[0] # read all pages
 tabula.read_pdf(pdf_file2, pages=1, stream=True)[0] # read 1 page
-tabula.read_pdf(pdf_file3, pages="1-2", stream=True)[0] #specific pages
+tabula.read_pdf(pdf_file4, pages="1-2", stream=True)[0] #specific pages
 
 tabula.convert_into(pdf_file1, "PDFTableExtraction_Output.csv", output_format="csv") # extract into file JSON, TSV, or CSV
 tabula.convert_into(pdf_file2, "PDFTableExtraction_Output.json", output_format="json")
 
+# 3 Read with Template
+
+template_path = "https://github.com/chezou/tabula-py/raw/master/tests/resources/data.tabula-template.json"
+tabula.read_pdf_with_template(pdf_file4, template_path)
 
 
      

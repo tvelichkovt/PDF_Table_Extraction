c9c40ec10337051ff4082a4ab9d494d16e343674
Todor <tvelichkovt@gmail.com>
2020-03-12 09:30:57 +0200

    PDFTableExtraction

--- PDFTableExtraction.py
+++ PDFTableExtraction.py
@@ -1,5 +1,7 @@
 '''
 #https://drive.google.com/drive/folders/1xjutthFIQ137VYSiDXBX6YtMUkt2sRoO
+#import os ; cwd = os.getcwd(); files = os.listdir(cwd) ; print("Files in %r: %s" % (cwd, files)) 
+
 '''
 
 # 1. Imports
@@ -7,37 +9,27 @@ import tabula
 from tabula import read_pdf
 
 # 2. Local fetch
-pdf_path = 'bgregistryagency3pages.pdf' #local patch
-tabula.read_pdf(pdf_path, pages="all", stream=True) #all pages
+pdf_file = 'PDFTableExtraction small table.pdf' #local patch
+tabula.read_pdf(pdf_file, pages="all", stream=True) #all pages
+tabula.convert_into(pdf_file, "PDFTableExtraction output.csv", output_format="csv")
+pdf_file = 'PDFTableExtraction Tesla Convertable Bonds.pdf' #local patch
+tabula.read_pdf(pdf_file, pages="1", stream=True) #specific pages
+tabula.convert_into(pdf_file, "PDFTableExtraction output.json", output_format="json")
 
-#tabula.read_pdf(pdf_path, pages="1", stream=True) #specific pages
-#tabula.read_pdf(pdf_path, pages=3, stream=True)[0] 
-#tabula.read_pdf(pdf_path, pages="1-2,3", stream=True) #specific pages
-#tabula.read_pdf(pdf_path, pages="all", stream=True, output_format="json") #all pages into JSON, TSV, or CSV
-#tabula.convert_into(pdf_path, "test.json", output_format="json") ..or convert
+#tabula.read_pdf(pdf_file, pages=3, stream=True)[0] 
+#tabula.read_pdf(pdf_file, pages="1-2,3", stream=True) #specific pages
+#tabula.read_pdf(pdf_file, pages="all", stream=True, output_format="json") #all pages into JSON, TSV, or CSV
+#tabula.convert_into(pdf_file, "test.json", output_format="json") ..or convert
 
+#3. www link fetch
+import tabula
+from tabula import read_pdf
 
+pdf_file_link = "http://www3.weforum.org/docs/GCR2017-2018/05FullReport/TheGlobalCompetitivenessReport2017%E2%80%932018.pdf"
 
-
-#1. Link fetch
-#pdf_path = "https://github.com/chezou/tabula-py/raw/master/tests/resources/data.pdf"
-
-import json
-
-with open('test.json') as f:
-  data = json.load(f)
-
-print(data)
-
-
-
-#===
-
-
-pdf_path3 = "https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/spanning_cells.pdf"
 tabula.read_pdf(
-    pdf_path3,
-    pages="1",
+    pdf_file_link,
+    pages="11",
     lattice=True,
     pandas_options={"header": [0, 1]},
     area=[0, 0, 50, 100],
@@ -48,7 +40,7 @@ tabula.read_pdf(
 
 
 template_path = "https://github.com/chezou/tabula-py/raw/master/tests/resources/data.tabula-template.json"
-tabula.read_pdf_with_template(pdf_path, template_path)
+tabula.read_pdf_with_template(pdf_file_link, template_path)
 
 
 
